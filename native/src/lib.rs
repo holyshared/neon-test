5fa12810db0d47ada14bdc75008cc3547d9f40ec
holyshared <holy.shared.design@gmail.com>
2016-04-15 14:26:14 +0900

    add hello2

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -2,13 +2,27 @@
 extern crate neon;
 
 use neon::vm::{ Call, JsResult };
-use neon::js::{ JsString };
+use neon::js::{ JsString, Variant };
 
 fn hello(call: Call) -> JsResult<JsString> {
     let scope = call.scope;
     Ok(JsString::new(scope, "hello node").unwrap())
 }
 
+fn hello2(call: Call) -> JsResult<JsString> {
+    let mut scope = call.scope;
+    let arguments = call.arguments;
+    let int_value = try!(arguments.require(scope, 0));
+
+    match int_value.variant() {
+        Variant::Number(v) => { println!("number: {:?}", v.value()) },
+        Variant::Integer(v) => { println!("integer: {:?}", v.value()); },
+        _ => { println!("oops!"); }
+    }
+    Ok(JsString::new(scope, "hello node").unwrap())
+}
+
 register_module!(m, {
-    m.export("hello", hello)
+//    m.export("hello", hello),
+    m.export("hello2", hello2)
 });
